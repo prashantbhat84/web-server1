f08f308d73334e5690311adb63e22b8992775cd1
Prashant Bhat <prashantbhat84@gmail.com>
2018-05-10 13:34:46 +0530

    initialise commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+var PORT = 3000;  // upper case variable names signifies constant and should not be changed
+var middleware = {
+  requireAuthentication: function(req,res,next){
+             console.log('private route hit');
+             next();
+             
+  },
+  logger: function(req,res,next){
+          
+
+               console.log(req.method + ' '+ req.originalUrl+ ' '+ new Date().toString());
+               next();
+               
+  }
+};
+//app.use(middleware.requireAuthentication); //application level middle ware
+app.use(middleware.logger);
+app.get('/about',middleware.requireAuthentication,function (req,res) {
+          res.send('about us');
+});
+app.use(express.static(__dirname+'/public'));
+//console.log(__dirname);
+
+app.listen(PORT,function () {
+       console.log('express server started at port : '+ PORT);
+          
+});
